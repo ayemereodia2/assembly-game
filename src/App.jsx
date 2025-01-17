30c252069b1cc284c3e3bce8a4fa59450b6db613
ayemereodia2 <odiadavid2@gmail.com>
2025-01-16 23:02:16 -0400

    refactor: update game title and enhance UI components for Assembly Game

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { languages } from "../languages";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [currentWords, setCurrentWords] = useState("react")
+
+  function HeaderView() {
+    return (
+      <header>
+        <span className="header-label">Assembly: Endgame</span>
+        <p className="header-para">
+          Guess the word in under 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+        <section className="game-status-container">
+          <h2>You win!</h2>
+          <p>“Farewell HTML & CSS” 🫡 </p>
+        </section>
+      </header>
+    );
+  }
+
+  const wordElement = currentWords.split("").map((character) => (
+      <span className="word-box">{character.toUpperCase()}</span>
+  ))
+
+  function WordView() {
+    return (
+      <section className="word-container">
+        {wordElement}
+      </section>
+    )
+  }
+
+  const languageElements = languages.map((language) => (
+    <div key={language.name}
+      style={{
+        backgroundColor: language.backgroundColor,
+        color: language.color,
+      }}
+      className="language-div"
+    >
+      <span>{language.name}</span>
+    </div>
+  ));
+
+  function LanguageViews() {
+    return (
+      <section className="languages-container">{languageElements}</section>
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <body>
+      <HeaderView />
+      <LanguageViews />
+      <WordView />
+    </body>
+  );
 }
-
-export default App
