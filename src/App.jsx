7c6fc8197f980787033ac7d8ecd7f54b17b2e1c5
ayemereodia2 <odiadavid2@gmail.com>
2025-01-17 19:05:32 -0400

    feat: implement keyboard interaction and enhance game UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { languages } from "../languages";
 
 export default function App() {
-  const [currentWords, setCurrentWords] = useState("react")
+  const mainWords = "rteact";
+  const alphabets = "abcdefghijklmnopqrstuvwxyz";
+  const [guesses, setGuesses] = useState([]);
+  const [isCorrect, setIsCorrect] = useState(Array(26).fill(false));
+  const wrongGuessCount = useRef(0);
+  const [keyboardStates, setKeyboardStates] = useState(Array(26).fill({
+    KEY_STATE: "key-button-box"
+  }));
 
+
+
+  let currentPos = useRef(0);
+  let remainderText = useRef(mainWords);
+  const [currentWords, setCurrentWords] = useState(Array(mainWords.length).fill(" "))
+  
+  const keyPressed = (char, id) => {
+    setGuesses(prev =>  (prev.includes(char) ? prev : [...prev, char]));
+    if(mainWords.split("").includes(char)){
+     
+       const index = remainderText.current.indexOf(char);
+       console.log(index);
+      remainderText.current = remainderText.current.replace(char, " ")
+
+      setCurrentWords((oldChar) => {
+        const updatedChars = [...oldChar]; // Create a copy of the old array
+        updatedChars[index] = char;       // Update the specific index
+        return updatedChars;              // Return the updated array
+      });
+
+      setIsCorrect(prev => [prev[id] = true, ...prev]);
+      setKeyboardStates(prevKeyState => [...prevKeyState, prevKeyState[id] = {KEY_STATE:  "key-button-box-correct"}])
+    }else {
+      wrongGuessCount.current = wrongGuessCount.current + 1;
+      setKeyboardStates(prevKeyState => [...prevKeyState, prevKeyState[id] = {KEY_STATE: "key-button-box-incorrect"}])
+      console.log(wrongGuessCount.current);
+    }
+  }
+  
   function HeaderView() {
     return (
       <header>
@@ -20,8 +56,12 @@ export default function App() {
     );
   }
 
-  const wordElement = currentWords.split("").map((character) => (
-      <span className="word-box">{character.toUpperCase()}</span>
+  const wordElement = currentWords.map((character, index) => (
+      <span key={index} className={"word-box"}>{character.toUpperCase()}</span>
+  ))
+
+  const keyBoardElements = alphabets.split("").map((char, id) => (
+    <button onClick={() => keyPressed(char, id)} key={char} className={keyboardStates[id].KEY_STATE}>{char.toUpperCase()}</button>
   ))
 
   function WordView() {
@@ -50,11 +90,21 @@ export default function App() {
     );
   }
 
+  function KeyboardView(){
+    return (
+      <section className="keyboard-container">
+        {keyBoardElements}
+      </section>
+    )
+  }
+
   return (
-    <body>
+    <div className="main-container">
       <HeaderView />
       <LanguageViews />
       <WordView />
-    </body>
+      <KeyboardView/>
+      <button className="base-button">New Game</button>
+    </div>
   );
 }
